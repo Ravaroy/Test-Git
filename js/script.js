ab328aca53aa5613a34531b7eaf603549baf11bb
Vovam <example@gmail.com>
2021-12-29 14:59:37 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// const answer = +prompt("Вам есть 18?", "18");
+
+// console.log(typeof(answer));
+// console.log(answer + 5);
+// console.log(typeof(answer)); 
+
+let incr = 10,
+    decr = 10;
+// incr++;
+// decr--;
+console.log(++incr);
+console.log(--decr);
